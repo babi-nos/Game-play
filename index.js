37d216fa8896e6b67f7dc9a8850ed245c2627181
Marvin <124067182+babi-nos@users.noreply.github.com>
2023-06-21 14:37:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -11,27 +11,34 @@ const btnLeft = document.querySelectorAll(".btn-left");
 const cardGameList = document.querySelector(".card-game-list");
 const cardGameList2 = document.querySelector(".card-game-list2");
 const cardGameList3 = document.querySelector(".card-game-list3");
+const cardGameList4 = document.querySelector(".card-game-list4");
 const lastChild = document.querySelector(".card-game-list li:last-child");
 const lastChild2 = document.querySelector(".card-game-list2 li:last-child");
 const lastChild3 = document.querySelector(".card-game-list3 li:last-child");
+const lastChild4 = document.querySelector(".card-game-list4 li:last-child");
 const ul = document.querySelectorAll(".all-game ul");
+const sliderContainer = document.querySelector(".slider");
+
 console.log(ul);
 
 let translate = 0;
 let translate2 = 0;
 let translate3 = 0;
+let translate4 = 0;
 const px = "px";
 const offset = 990; // Valeur de décalage (peut être ajustée selon vos besoins)
 const largeurEcran = window.innerWidth;
 const width = cardGameList.scrollWidth;
 const width2 = cardGameList2.scrollWidth;
 const width3 = cardGameList3.scrollWidth;
+const width4 = cardGameList4.scrollWidth;
 const diffenrence = width - largeurEcran;
 const diffenrence2 = width2 - largeurEcran;
 const diffenrence3 = width3 - largeurEcran;
+const diffenrence4 = width4 - largeurEcran;
 
 // const largeurEcran = window.innerWidth;
-const pourcentage = 17;
+const pourcentage = 19;
 const dixSeptPourcent = Math.round((pourcentage / 100) * largeurEcran);
 console.log(dixSeptPourcent);
 
@@ -40,18 +47,12 @@ const forChoice = document.querySelector(".forChoice");
 
 btnNavbar.addEventListener("click", () => {
   btnNavbar.classList.toggle("clicked");
-  // observer();
 });
 
 navItem.addEventListener("click", () => {
   if (toggleNav.classList[1] == "clicked") {
     main.style.gridTemplateColumns = "17% 83%";
     navChoice.style.left = "0px";
-
-    // btnRight[0].style.display = "none";
-
-    // cardGameList.style.left += "-250px";
-    // translate = -diffenrence;
   } else {
     main.style.gridTemplateColumns = "0% 100%";
     navChoice.style.left = "-500px";
@@ -111,35 +112,47 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       if (entry.target === lastChild) {
+        cardGameList.style.transition = "all 0.4s ease-out";
         btnRight[0].style.display = "none";
         cardGameList.style.left = `${-diffenrence + -40}px`;
         translate = -diffenrence;
         console.log(`${-diffenrence + -40}px`);
-        // if (toggleNav.classList[1] == "clicked") {
-        // cardGameList.style.left = `${-diffenrence - dixSeptPourcent}px`;
-        // } else {
-        // cardGameList.style.left = `${-diffenrence + -40}px`;
-        // }
+        if (toggleNav.classList[1] == "clicked") {
+          cardGameList.style.left = `${-diffenrence - dixSeptPourcent}px`;
+        } else {
+          cardGameList.style.left = `${-diffenrence + -40}px`;
+        }
       } else if (entry.target === lastChild2) {
+        cardGameList2.style.transition = "all 0.4s ease-out";
         btnRight[1].style.display = "none";
         cardGameList2.style.left = `${-diffenrence2 + -40}px`;
         translate2 = -diffenrence2;
 
-        // if (toggleNav.classList[1] == "clicked") {
-        // cardGameList2.style.left = `${-diffenrence2 - dixSeptPourcent}px`;
-        // } else {
-        // cardGameList2.style.left = `${-diffenrence2 + -40}px`;
-        // }
+        if (toggleNav.classList[1] == "clicked") {
+          cardGameList2.style.left = `${-diffenrence2 - dixSeptPourcent}px`;
+        } else {
+          cardGameList2.style.left = `${-diffenrence2 + -40}px`;
+        }
       } else if (entry.target === lastChild3) {
+        cardGameList3.style.transition = "all 0.4s ease-out";
         btnRight[2].style.display = "none";
         cardGameList3.style.left = `${-diffenrence3 + -40}px`;
         translate3 = -diffenrence3;
-
-        // if (toggleNav.classList[1] == "clicked") {
-        // cardGameList3.style.left = `${-diffenrence3 - dixSeptPourcent}px`;
-        // } else {
-        // cardGameList3.style.left = `${-diffenrence3 + -40}px`;
-        // }
+        if (toggleNav.classList[1] == "clicked") {
+          cardGameList3.style.left = `${-diffenrence3 - dixSeptPourcent}px`;
+        } else {
+          cardGameList3.style.left = `${-diffenrence3 + -40}px`;
+        }
+      } else if (entry.target === lastChild4) {
+        cardGameList4.style.transition = "all 0.4s ease-out";
+        btnRight[3].style.display = "none";
+        cardGameList4.style.left = `${-diffenrence4 + -40}px`;
+        translate3 = -diffenrence4;
+        if (toggleNav.classList[1] == "clicked") {
+          cardGameList4.style.left = `${-diffenrence4 - dixSeptPourcent}px`;
+        } else {
+          cardGameList4.style.left = `${-diffenrence4 + -40}px`;
+        }
       }
     } else {
       if (entry.target === lastChild) {
@@ -149,6 +162,8 @@ const observer = new IntersectionObserver((entries) => {
         btnRight[1].style.display = "block";
       } else if (entry.target === lastChild3) {
         btnRight[2].style.display = "block";
+      } else if (entry.target === lastChild4) {
+        btnRight[3].style.display = "block";
       }
     }
   });
@@ -157,23 +172,29 @@ const observer = new IntersectionObserver((entries) => {
 btnRight.forEach((btn) => {
   btn.addEventListener("click", function (e) {
     if (this.dataset.value == 0) {
-      cardGameList.style.transition = "all 1.1s ease-out";
+      cardGameList.style.transition = "all 0.7s ease-in-out";
       translate -= offset;
       cardGameList.style.left = translate + px;
       btnLeft[0].style.display = "block";
       observer.observe(lastChild, translate);
     } else if (this.dataset.value == 1) {
-      cardGameList2.style.transition = "all 1.1s ease-out";
+      cardGameList2.style.transition = "all 0.7s ease-out";
       translate2 -= offset;
       cardGameList2.style.left = translate2 + px;
       btnLeft[1].style.display = "block";
       observer.observe(lastChild2);
     } else if (this.dataset.value == 2) {
-      cardGameList3.style.transition = "all 1.1s ease-out";
+      cardGameList3.style.transition = "all 0.7s ease-out";
       translate3 -= offset;
       cardGameList3.style.left = translate3 + px;
       btnLeft[2].style.display = "block";
       observer.observe(lastChild3);
+    } else if (this.dataset.value == 3) {
+      cardGameList4.style.transition = "all 0.7s ease-out";
+      translate4 -= offset;
+      cardGameList4.style.left = translate4 + px;
+      btnLeft[3].style.display = "block";
+      observer.observe(lastChild4);
     }
   });
 });
@@ -181,7 +202,7 @@ btnRight.forEach((btn) => {
 btnLeft.forEach((btnL) => {
   btnL.addEventListener("click", function (e) {
     if (this.dataset.value == 0) {
-      cardGameList.style.transition = "all 1.1s ease-out";
+      cardGameList.style.transition = "all 0.7s ease-out";
       cardGameList.style.left = `${translate}px`;
       cardGameList.style.justifyContent = "left";
       translate += offset; // ATTENTION ORDRE IMPORTANT
@@ -192,7 +213,7 @@ btnLeft.forEach((btnL) => {
         cardGameList.style.left = "2%";
       }
     } else if (this.dataset.value == 1) {
-      cardGameList2.style.transition = "all 1.1s ease-out";
+      cardGameList2.style.transition = "all 0.7s ease-out";
       cardGameList2.style.left = `${translate2}px`;
       cardGameList2.style.justifyContent = "left";
       translate2 += offset;
@@ -203,7 +224,7 @@ btnLeft.forEach((btnL) => {
         btnLeft[1].style.display = "none";
       }
     } else if (this.dataset.value == 2) {
-      cardGameList3.style.transition = "all 1.1s ease-out";
+      cardGameList3.style.transition = "all 0.7s ease-out";
       cardGameList3.style.left = `${translate3}px`;
       cardGameList3.style.justifyContent = "left";
       translate3 += offset;
@@ -212,6 +233,17 @@ btnLeft.forEach((btnL) => {
       if (translate3 >= 0) {
         cardGameList3.style.left = "2%";
         btnLeft[2].style.display = "none";
+      }
+    } else if (this.dataset.value == 3) {
+      cardGameList4.style.transition = "all 0.7s ease-out";
+      cardGameList4.style.left = `${translate4}px`;
+      cardGameList4.style.justifyContent = "left";
+      translate4 += offset;
+      cardGameList4.style.left = translate4 + px;
+
+      if (translate4 >= 0) {
+        cardGameList4.style.left = "2%";
+        btnLeft[3].style.display = "none";
       }
     }
   });
@@ -233,6 +265,10 @@ const showArrow = (containerGame) => {
       btnLeft[2].style.opacity = 1;
       btnRight[2].style.opacity = 1;
       break;
+    case "Action":
+      btnLeft[3].style.opacity = 1;
+      btnRight[3].style.opacity = 1;
+      break;
     default:
       break;
   }
@@ -251,6 +287,8 @@ rowsGame.forEach((rows) => {
       btnRight[1].style.opacity = 0;
       btnLeft[2].style.opacity = 0;
       btnRight[2].style.opacity = 0;
+      btnLeft[3].style.opacity = 0;
+      btnRight[3].style.opacity = 0;
     });
   });
 });
@@ -266,6 +304,7 @@ const containerCategory = document.querySelectorAll(".container-card-netflix");
 
 function displayCategory(nameCategory) {
   containerCategory.forEach(function (containerCate) {
+    sliderContainer.style.display = "none";
     containerCate.style.display = "block"; // Réinitialiser la propriété display pour tous les éléments
     const changeDisplay = containerCate.querySelector(".displayChoice");
     const title = containerCate.querySelector(".title");
@@ -285,14 +324,19 @@ function displayCategory(nameCategory) {
     changeDisplay.style.justifyContent = "center";
     changeDisplay.style.left = "0%";
 
+    main.style.gridTemplateColumns = "0% 100%";
+    navChoice.style.left = "-500px";
+    btnNavbar.classList.remove("clicked");
 
     if (nameCategory !== cateDisplay) {
       containerCate.style.display = "none";
     }
     if (nameCategory === "Accueil") {
-      translate = 0;
-      translate2 = 0;
-      translate3 = 0;
+      // translate = 0;
+      // translate2 = 0;
+      //translate3 = 0;
+
+      sliderContainer.style.display = "block";
 
       containerCate.style.display = "block";
       changeDisplay.style.display = "flex";
@@ -315,11 +359,7 @@ allLink.forEach((link) => {
   link.addEventListener("click", (e) => {
     const nameCategory = link.querySelector("a").lastChild.data;
     e.preventDefault();
-    // observer.unobserve(lastChild);
-    // observer.unobserve(lastChild2);
-    // observer.unobserve(lastChild3);
     observer.disconnect();
-
 
     displayCategory(nameCategory);
   });
